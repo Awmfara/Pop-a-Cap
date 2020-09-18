0ba4591c200b8fd794c058f35fa6f82ab12c5853
Awmfara <69292567+Awmfara@users.noreply.github.com>
2020-09-18 12:09:22 +1000

    Creeped Up

---
+++ Assets/Scripts/Playa.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Playa : MonoBehaviour
+{
+    [Header("Playa Settings")]
+    private float playaLevel = 0;
+    private float playaXp = 0;
+    private float clickAmount = 0;
+    private float initCaps = 0;
+
+    private void Update()
+    {
+        
+    }
+
+
+
+
+}
