26b1f1f06de45cfa312c6c7df6e9691316d5e3f9
Awmfara <69292567+Awmfara@users.noreply.github.com>
2020-09-22 09:33:05 +1000

    Major Changes
    Redefined scripots
    added assetts

---
+++ Assets/Scripts/ItemAvailability.cs
@@ -0,0 +1,101 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using Player;
+
+namespace Itemas
+{
+    public class ItemAvailability : MonoBehaviour
+    {
+        [Header("Playa Script")]
+        [SerializeField, Tooltip("Attach to playaScript")]
+        private Playa playa = null;
+
+        [Header("Buttons")]
+        [SerializeField, Tooltip("Attach to Button (On Click Functionality)")]
+        private Button itemBut = null;
+        [SerializeField, Tooltip("Attach to Button (Availability Functionality")]
+        private GameObject nextItemBut = null;
+        [Header("Price Variables")]
+        [SerializeField, Tooltip("Attach to  itemBut/Price/costText Text")]
+        private Text costText = null;
+        [SerializeField, Tooltip("Attach to  itemSold/Price/Sold Text")]
+        private Text soldText = null;
+        [SerializeField, Tooltip("Attach to ItemSold Object")]
+        private GameObject itemSold = null;
+        [SerializeField, Tooltip("Cost of Meow Meow")]
+        private int itemCost = 10;
+
+
+        [Header("Settings for Generic Items")]
+
+        [SerializeField,Tooltip("Amount of Item that exists")]
+        private int itemCount = 0;
+        [SerializeField, Tooltip("Score Multiplies By(Generic Option, Set to 0 if other behavior defined")]
+        private int multiplier = 0;
+        [SerializeField,Tooltip("Don't change watch countdown")]
+        private float currentTime = 5;
+        [SerializeField, Tooltip("Time between clicks that item repeats")]
+        private float currentStartTime = 10;
+
+        private void Start()
+        {
+            itemBut.onClick.AddListener(ButPress);
+        }
+        private void Update()
+        {
+            ItemAvailMeth();
+
+            GenericItemScorerMeth();
+
+        }
+        /// <summary>
+        /// Adds multiplier to score and Moolah
+        /// Use for items without specific behaviors,
+        /// For items with specific behaviors set multiplier to 0
+        /// </summary>
+        private void GenericItemScorerMeth()
+        {
+            if (currentTime <= 0)
+            {
+                playa.moolah += itemCount * multiplier;
+                playa.scoreValue += itemCount * multiplier;
+                currentTime = currentStartTime;
+            }
+            else
+            {
+                currentTime -= Time.deltaTime;
+            }
+        }
+
+        /// <summary>
+        /// Sets the Cost Number in Text to Item Cost,
+        /// Changes Item Availability based on Moolah availability,
+        /// After Item becomes first avaialble, sold out icon appears when out of Moolah
+        /// </summary>
+        private void ItemAvailMeth()
+        {
+            costText.text = "" + itemCost;
+            soldText.text = "" + itemCost;
+
+            if (playa.moolah >= itemCost)
+            {
+                nextItemBut.SetActive(true);
+                itemSold.SetActive(true);
+            }
+            if (playa.moolah < itemCost)
+            {
+                nextItemBut.SetActive(false);
+            }
+        }
+        /// <summary>
+        /// Spends Moolah On Button press, Adds item count for Generic Items
+        /// </summary>
+        void ButPress()
+        {
+            playa.moolah -= itemCost;
+            itemCount += 1;
+        }
+    }
+}
--- Assets/Scripts/Playa.cs
+++ Assets/Scripts/Playa.cs
@@ -2,64 +2,60 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
-[RequireComponent(typeof(AudioSource))]
-public class Playa : MonoBehaviour
+using Itemas;
+
+namespace Player
 {
-    
-    [Header("Buttons")]
-    public Button cap;
-    
-    [Header("Playa Settings")]
-    private float playaLevel = 0;
-    [SerializeField]
-    private float playaXp = 0;
-    [SerializeField]
-    private float clickAmount = 0;
-    private float initCaps = 0;
-    public Rect audiorect;
-
- 
-
-    public AudioSource buttonSound;
-    public AudioClip xylo;
-    public float volume = 0.5f;
-    private void Start()
+    [RequireComponent(typeof(AudioSource))]
+    public class Playa : MonoBehaviour
     {
-        audiorect = new Rect(20, 20, 100, 20);
-        cap.onClick.AddListener(butPress);
-       
-    }
 
-    private void Update()
-    {
-        if (Input.GetMouseButtonDown(0))
+        [Header("Sam")]
+        public Button sam;
+
+        [SerializeField, Tooltip("Currency")]
+        public int moolah = 0;
+        public Text moolahCounter;
+        public int scoreValue = 0;
+        public Text score;
+
+        [Header("Sound FX")]
+        public AudioSource buttonSound;
+        public AudioClip faceSound;
+        public float volume = 0.5f;
+
+
+
+
+        private void Start()
         {
-  buttonSound.PlayOneShot(xylo, volume);
-        }
- 
-        if (clickAmount>=10)
-        {
-            playaXp++;
-            clickAmount = 0;
+            sam.onClick.AddListener(butPress);
 
         }
-    }
-    void butPress()
-    {
-      
-        clickAmount++;
-      
+
+        private void Update()
+        {
+
+            Counters();
+
+        }
+
+
+        /// <summary>
+        /// Displays Score and Mullah Amount 
+        /// </summary>
+        private void Counters()
+        {
+            score.text = "" + scoreValue;
+            moolahCounter.text = "" + moolah;
+        }
+        void butPress()
+        {
+            moolah++;
+            scoreValue++;
+            buttonSound.PlayOneShot(faceSound, volume);
+
+        }
 
     }
-
-    private void OnDrawGizmos()
-    {
-        Gizmos.color = Color.blue;
-        DrawRect(audiorect);
-    }
-    private void DrawRect(Rect rect)
-    {
-        Gizmos.DrawWireCube(new Vector3(rect.center.x, rect.center.y, 0.01f), new Vector3(rect.size.x, rect.size.y, 0.01f));
-    }
-   
 }
